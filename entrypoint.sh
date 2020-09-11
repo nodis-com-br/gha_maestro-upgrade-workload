775c3d94765f6fec362b131d0ef5302258a424a5
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-09-10 21:23:09 -0300

    refactoring

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
-KUBECONFIG_FILE="kubeconfig"
-KUBECTL_OPTIONS="-v=6 --kubeconfig=${KUBECONFIG_FILE} -n ${NODIS_K8S_NAMESPACE} --context ${NODIS_DEPLOY_ENV}"
-
-export CHARTMUSEUM_URI="https://${NODIS_CHART_REPOSITORY_USER}:${NODIS_CHART_REPOSITORY_PASSWORD}@${NODIS_CHART_REPOSITORY_HOST}"
+[[ ${NODIS_DEPLOY_ENV} == "qa" && ${DEPLOY_QA_TO_PROD} != "false" ]] && NODIS_DEPLOY_ENV="prod"
 pip install -i "https://${NODIS_PYPI_USER}:${NODIS_PYPI_PASSWORD}@${NODIS_PYPI_HOST}/simple" maestro
 
-maestro export_kubeconfig ${KUBECONFIG_FILE}
+git clone https://${GITHUB_TOKEN}:@github.com/${GITHUB_ORGANIZATION}/${MAESTRO_REPOSITORY}.git
+cd ${MAESTRO_REPOSITORY}
 
-kubectl ${KUBECTL_OPTIONS} config get-contexts
-kubectl ${KUBECTL_OPTIONS} scale ${NODIS_SERVICE_TYPE}/${NODIS_SERVICE_NAME} --current-replicas=0 --replicas=1 || true
-kubectl ${KUBECTL_OPTIONS} set image ${NODIS_SERVICE_TYPE}/${NODIS_SERVICE_NAME} ${NODIS_SERVICE_NAME}=${NODIS_IMAGE_NAME}:${NODIS_FULL_VERSION}
+RESOURCE_FILE=`find . -name ${NODIS_PROJECT_NAME}.yaml`
+NEW_VALUES=`printf '{"%s":{"image": {"tag": "%s"}}}' ${NODIS_DEPLOY_ENV} ${NODIS_PROJECT_VERSION}`
+
+case `echo ${RESOURCE_FILE} | wc -w` in
+    1) maestro edit_values ${RESOURCE_FILE} -v ${NEW_VALUES}
+       maestro -e ${NODIS_DEPLOY_ENV} upgrade ${RESOURCE_FILE}
+       git add ${RESOURCE_FILE}
+       git commit -m "Updated ${RESOURCE_FILE} ${NODIS_DEPLOY_ENV} tag: ${NODIS_PROJECT_VERSION} - skip_ci"
+       git push;;
+    0) echo "Resource file for ${NODIS_PROJECT_NAME} not found" && exit 1;;
+    *) echo "Multiple resource files found: ${RESOURCE_FILE}" && exit 1;;
+esac
+
