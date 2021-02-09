ee36a963f5cf0b3aa6fb17a8d7b90df7ba09c3f5
Pedro Tonini <pedro.tonini@nodis.com.br>
2021-02-09 11:37:47 -0300

    Altered discord notification

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,4 +28,4 @@ case `echo ${RESOURCE_FILE} | wc -w` in
 esac
 
 curl --location --header 'Content-Type: application/json' --request POST 'https://discord.com/api/webhooks/740966411366563870/k34Nc5ee0_j1OAzNNRNxGnUiUTicAX3CAYyjtyZXd9Q395NTHZUqlxWf31JOdTSUhfc6' \
-     --data-raw '{"embeds":[{"title":"Application deployed!","description":"The application **'${NODIS_PROJECT_NAME}'** was deployed with the version **'${NODIS_PROJECT_VERSION}'**","color":3311947,"fields":[{"name":"Triggered by","value":"'${GITHUB_ACTOR}'","inline":true},{"name":"Environment","value":"'${NODIS_DEPLOY_ENV}'","inline":true},{"name":"Repo","value":"https://'${GITHUB_SERVER_URL}'/'${GITHUB_REPOSITORY}'","inline":true}]}]}'
+     --data-raw '{"embeds":[{"title":"Application deployed!","description":"The application **'${NODIS_PROJECT_NAME}'** was deployed with the version **'${NODIS_PROJECT_VERSION}'**","color":3311947,"fields":[{"name":"Triggered by","value":"'${GITHUB_ACTOR}'","inline":true},{"name":"Environment","value":"'${NODIS_DEPLOY_ENV}'","inline":true},{"name":"Repo","value":"'${GITHUB_SERVER_URL}'/'${GITHUB_REPOSITORY}'","inline":true}]}]}'
